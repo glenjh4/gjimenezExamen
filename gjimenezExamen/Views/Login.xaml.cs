0425e6d43c22ed461855e93b9d0cb51b74c5f90d
master <master@SMLAB306PC12_25>
2024-10-25 20:50:43 -0500

    Avances

--- gjimenezExamen/Views/Login.xaml.cs
+++ gjimenezExamen/Views/Login.xaml.cs
@@ -2,28 +2,31 @@ namespace gjimenezExamen.Views;
 
 public partial class Login : ContentPage
 {
-    string[,] datos = { { "estudiante", "uisrael" }, { "moviles", "2024" } };
-    public Login()
-	{
-		InitializeComponent();
-	}
-    private async void OnLoginClicked(object sender, EventArgs e)
-    {
-        string username = usernameEntry.Text;
-        string password = passwordEntry.Text;
+    //Listas de usuarios y contraseñas
+    string[] users = { "estudiante2024", "examen1", "parcial1" };
+    string[] passwords = { "uisrael2024", "parcial1", "2024" };
 
-        if (users.ContainsKey(username) && users[username] == password)
+    public Login()
+    {
+        InitializeComponent();
+        this.datoU = dato;
+        lblDato.Text = "usuario conectado:" + dato;
+    }
+    private async void OnIngresarClicked(object sender, EventArgs e)
+    {
+        string usuario = txtUsuario.Text;
+        string contrasena = txtContrasena.Text;
+
+        int userIndex = Array.IndexOf(users, usuario);
+
+        if (userIndex != -1 && passwords[userIndex] == contrasena)
         {
-            await Navigation.PushAsync(new RegistrationPage(username));
+            await DisplayAlert("Acceso Exitoso", $"¡Bienvenido {users[userIndex]}!", "ACEPTAR");
+            Application.Current.MainPage = new Registro();
         }
         else
         {
-            await DisplayAlert("Error", "Datos incorrectos", "OK");
+           IngresarMessage.Text = "Usuario o contraseña incorrectos";
         }
-    }
-
-    private async void OnAboutClicked(object sender, EventArgs e)
-    {
-        await Navigation.PushAsync(new AboutPage());
     }
 }
